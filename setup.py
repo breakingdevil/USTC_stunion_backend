f2d170b6c163a6b54614d5d215da4cf4e87a0f21
iBug <7273074+iBug@users.noreply.github.com>
2019-02-25 14:21:59 +0800

    prepare pytest-flask

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
-
+#!/usr/bin/env python3
 # gunicorn.py
 import logging
 import logging.handlers
 from logging.handlers import WatchedFileHandler
 import os
 import multiprocessing
-bind = '0.0.0.0:5000'      #绑定ip和端口号
-backlog = 512                #监听队列
-  #gunicorn要切换到的目的工作目录
-timeout = 30      #超时
-worker_class = 'sync' #使用gevent模式，还可以使用sync 模式，默认的是sync模式
+
+bind = '0.0.0.0:5000'  # 绑定ip和端口号
+backlog = 512  # 监听队列
+  # gunicorn要切换到的目的工作目录
+timeout = 30  # 超时
+worker_class = 'sync'  # 使用gevent模式，还可以使用sync 模式，默认的是sync模式
 
 workers = multiprocessing.cpu_count() * 2 + 1    #进程数
-threads = 2 #指定每个进程开启的线程数
-loglevel = 'info' #日志级别，这个日志级别指的是错误日志的级别，而访问日志的级别无法设置
-access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" "%({X-Real-IP}i)s"'    #设置gunicorn访问日志格式，错误日志无法设置
+threads = 2  # 指定每个进程开启的线程数
+loglevel = 'info'  # 日志级别，这个日志级别指的是错误日志的级别，而访问日志的级别无法设置
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" "%({X-Real-IP}i)s"'  # 设置gunicorn访问日志格式，错误日志无法设置
 
 """
 其每个选项的含义如下：
@@ -33,5 +34,5 @@ L          request time in decimal seconds
 p          process ID
 """
 
-accesslog = "/var/log/gunicorn_access.log"      #访问日志文件
-errorlog = "/var/log/gunicorn_error.log"        #错误日志文件
+accesslog = "/var/log/gunicorn_access.log"  # 访问日志文件
+errorlog = "/var/log/gunicorn_error.log"  # 错误日志文件
