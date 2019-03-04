095df0b0fd12c51918eabce1b314077c40b4e897
iBug <7273074+iBug@users.noreply.github.com>
2019-03-04 14:08:29 +0800

    working framework

--- run.sh
+++ run.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-source ./config.sh
+if [ -r ./config.sh ]; then
+  source ./config.sh
+else
+  source ./config-sample.sh
+fi
+
+for var in DB_NAME USER; do
+  if [ -z "${!var}" ]; then
+    echo "$var not set in config.sh, aborting" >&2
+    exit 1
+  fi
+done
 
 run_mysql() {
   if [ -n "$PASSWORD" ]; then
-    mysql -u"$USER" -p"$PASSWORD" -D "$DB_NAME" "$@"
+    mysql -u"$USER" -p"$PASSWORD" -D"$DB_NAME" "$@"
   else # Assume no password
-    mysql -u"$USER" -D "$DB_NAME" "$@"
+    mysql -u"$USER" -D"$DB_NAME" "$@"
   fi
 }
 
 run() {
+  local FILE="$1"
+  run_mysql -Ns < "$FILE"
 }
